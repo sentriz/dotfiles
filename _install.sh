0bf37a64d6a063d32d05ed1667e4d114c6cd6a2d
sentriz <senan.f.b.kelly+github@gmail.com>
2015-08-19 05:05:33 +0100

    add dotfile installer

---
+++ _install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+dir=~/dotfiles
+old_dir=~/dotfiles_old
+dotfiles="bashrc vimrc vim screen gitconfig"
+
+mkdir -p $old_dir
+
+for file in $dotfiles; do
+    echo "moving ~/.$file to $old_dir"
+    mv ~/.$file $old_dir
+    echo "creating symlink ~/.$file => $dir/$file"
+    ln -s $dir/$file ~/.$file
+done
