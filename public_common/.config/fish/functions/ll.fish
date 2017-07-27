5a9862ce7a277ca2072f224916220b3757c35cb4
sentriz <senan.f.b.kelly+github@gmail.com>
2017-07-27 16:08:18 +0100

    update listing funcs

--- public_common/.config/fish/functions/ll.fish
+++ public_common/.config/fish/functions/ll.fish
@@ -1,4 +1,4 @@
 function ll
-    ls -vFqrlothA --color=yes  --time-style=long-iso $argv \
-    | sed "s/(date +%Y-%m-%d)/\x1b[32m     TODAY\x1b[m/; s/(date +'%Y-%m-%d' -d yesterday)/\x1b[33m YESTERDAY\x1b[m/"
+	ls -vFqrlothA --color=yes  --time-style=long-iso $argv \
+    | sed "s/"(date +%Y-%m-%d)"/\x1b[32m     TODAY\x1b[m/; s/"(date +'%Y-%m-%d' -d yesterday)"/\x1b[33m YESTERDAY\x1b[m/"
 end
