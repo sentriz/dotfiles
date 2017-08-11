a68784617e70c8a71c877a6a868e2383c1cf9e8d
sentriz <senan.f.b.kelly+github@gmail.com>
2017-08-11 16:05:28 +0100

    move xlogin to functuion

--- public_common/.config/fish/manjaro.fish
+++ public_common/.config/fish/manjaro.fish
@@ -8,7 +8,6 @@ set -x JAVA_HOME /usr/lib/jvm/java-8-openjdk/
 set -x PATH $HOME/.local/lib/python3.5/site-packages/ $PATH
 set -x PATH /opt/android-sdk/platform-tools/ $PATH
 
-# start x
 if status --is-login; and test -z $DISPLAY; and test -z $TMUX
-    source $HOME/.config/fish/xlogin.fish
+    xlogin
 end
