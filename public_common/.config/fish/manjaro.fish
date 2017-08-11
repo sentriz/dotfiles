9d89f63e1ccc40b2adc1867ea5ac837e3589644a
sentriz <senan.f.b.kelly+github@gmail.com>
2017-08-11 16:00:00 +0100

    change manjaro.fish order

--- public_common/.config/fish/manjaro.fish
+++ public_common/.config/fish/manjaro.fish
@@ -1,12 +1,12 @@
 #!/usr/bin/env fish
 
+# extra config
+set -x host_prompt_colour 'green'
+
 # extra path
 set -x JAVA_HOME /usr/lib/jvm/java-8-openjdk/
 set -x PATH $HOME/.local/lib/python3.5/site-packages/ $PATH
 set -x PATH /opt/android-sdk/platform-tools/ $PATH
-
-# extra config
-set -x host_prompt_colour 'green'
 
 # start x
 if status --is-login; and test -z $DISPLAY; and test -z $TMUX
