ff8883cad36441ff530b3573c1cea1da30fd4b9d
sentriz <senan.f.b.kelly+github@gmail.com>
2017-08-21 23:58:02 +0100

    fix ruby stuff in manjaro.fish

--- public_common/.config/fish/manjaro.fish
+++ public_common/.config/fish/manjaro.fish
@@ -3,10 +3,19 @@
 # extra config
 set -x host_prompt_colour 'green'
 
-# extra path
+# java
 set -x JAVA_HOME /usr/lib/jvm/java-8-openjdk/
-set -x PATH $HOME/.local/lib/python3.5/site-packages/ $PATH
+
+# python path
+set -x PATH $HOME/.local/lib/python*/site-packages/ $PATH
+
+# adb path
 set -x PATH /opt/android-sdk/platform-tools/ $PATH
+
+# ruby path
+set -x GEM_HOME (ls -t -U | ruby -e 'puts Gem.user_dir')
+set -x GEM_PATH $GEM_HOME
+set -x PATH $GEM_HOME/bin $PATH
 
 if status --is-login; and test -z $DISPLAY; and test -z $TMUX
     xlogin
