a73395fac351886b67c6b0dce3bdd4a8ea19ab39
sentriz <senan.f.b.kelly+github@gmail.com>
2017-08-22 01:02:40 +0100

    do less in config.fish that shouldnt be done on all machines

--- public_common/.config/fish/config.fish
+++ public_common/.config/fish/config.fish
@@ -4,17 +4,11 @@ set -x PATH /usr/local/bin                   $PATH
 set -x PATH /usr/sbin /sbin                  $PATH
 set -x PATH /bin /usr/bin                    $PATH
 set -x PATH $HOME/.local/bin                 $PATH
-set -x PATH $HOME/.local/lib/python3.6/site-packages/ $PATH
 
 # recurisve ~/bin in path
 if test -d $HOME/bin
     set -x PATH (find -L $HOME/bin -type d) $PATH
 end
-
-# sourcy stuff
-source $HOME/.keychain/(hostname)-fish > /dev/null 2>&1
-source $HOME/.keychain/(hostname)-fish-gpg > /dev/null 2>&1
-eval (python3 -m virtualfish auto_activation)
 
 # host specific
 if test -f $HOME/.config/fish/(hostname).fish
--- public_common/.config/fish/shmig.fish
+++ public_common/.config/fish/shmig.fish
@@ -2,3 +2,8 @@
 
 # extra config
 set -x host_prompt_colour 'red'
+
+# sourcy stuff
+source $HOME/.keychain/(hostname)-fish > /dev/null 2>&1
+source $HOME/.keychain/(hostname)-fish-gpg > /dev/null 2>&1
+eval (python3 -m virtualfish auto_activation)
