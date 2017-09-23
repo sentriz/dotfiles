5d2f8621aa2a14383a1dfc22d9cda3df9d02e544
sentriz <senan.f.b.kelly+github@gmail.com>
2017-09-23 15:07:14 +0100

    add sourcey stuff to ben box

--- public_common/.config/fish/BenMcMahon.fish
+++ public_common/.config/fish/BenMcMahon.fish
@@ -2,3 +2,8 @@
 
 # extra config
 set -x host_prompt_colour 'cyan'
+
+# sourcy stuff
+source $HOME/.keychain/(hostname)-fish > /dev/null 2>&1
+source $HOME/.keychain/(hostname)-fish-gpg > /dev/null 2>&1
+eval (python3.6 -m virtualfish auto_activation)
