628368a651091fc4c7b4c1d6e57984e7f0f9118d
sentriz <senan.f.b.kelly+github@gmail.com>
2017-10-13 15:50:36 +0100

    update right pr

--- public_common/.config/fish/functions/fish_right_prompt.fish
+++ public_common/.config/fish/functions/fish_right_prompt.fish
@@ -1,4 +1,17 @@
-function fish_right_prompt
-  printf '%s' (__fish_git_prompt)
-  set_color normal
+# Defined in /tmp/fish.VJZqFS/fish_right_prompt.fish @ line 2
+function fish_right_prompt --description 'Write out the git right prompt'
+	set -l branch (git symbolic-ref --quiet --short HEAD 2> /dev/null)
+    if test -z "$branch"
+        return
+    end
+    if test "$branch" = master
+        set_color brred
+    else
+        set_color brgreen
+    end
+    echo "$branch"
+    set_color normal
+    if git diff-index --quiet HEAD --
+        echo " clean"
+    end
 end
