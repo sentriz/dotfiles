047d58a18fbebd38fea0dee99308025545c400e3
sentriz <senan.f.b.kelly+github@gmail.com>
2017-10-18 10:45:35 +0100

    remove chromium from xlogin

--- public_common/.config/fish/functions/xlogin.fish
+++ public_common/.config/fish/functions/xlogin.fish
@@ -1,6 +1,6 @@
 function xlogin 
     dialog --colors --ok-label "run" --cancel-label "logout" --menu "select session" \
-        0 0 0 "i3wm" "" "chromium" "" "fish" "" "ncmpcpp" "" "kodi" "" "shutdown" "" 2> /tmp/loginoption
+        0 0 0 "i3wm" "" "fish" "" "ncmpcpp" "" "kodi" "" "shutdown" "" 2> /tmp/loginoption
     if test ! $status -eq 0
         kill %self
     end
@@ -8,8 +8,6 @@ function xlogin
     switch (cat /tmp/loginoption)
         case i3wm
             eval "exec startx $HOME/.xinitrc_iii -- -keeptty -nolisten tcp 2> /dev/null 1>&2"
-        case chromium
-            eval "exec startx $HOME/.xinitrc_chr -- -keeptty -nolisten tcp 2> /dev/null 1>&2"
         case kodi
             eval "exec startx $HOME/.xinitrc_kodi -- -keeptty -nolisten tcp 2> /dev/null 1>&2"
         case fish
