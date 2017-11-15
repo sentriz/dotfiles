01f025c66700fe963376e1c4a2e4f38325f94dce
sentriz <senan@senan.xyz>
2017-11-15 01:40:17 +0000

    add status 🅱lack to prompt

--- public_common/.config/fish/functions/fish_prompt.fish
+++ public_common/.config/fish/functions/fish_prompt.fish
@@ -1,9 +1,11 @@
-# Defined in /tmp/fish.rpsWbS/fish_prompt.fish @ line 2
+# Defined in /tmp/fish.b3iMas/fish_prompt.fish @ line 2
 function fish_prompt
-    set -l items (date +%A | tr '[:upper:]' '[:lower:]') \
-                 (set_color "$host_colour")(hostname)(set_color normal) \
-                 (set -q VIRTUAL_ENV; and basename "$VIRTUAL_ENV env") \
-                 (prompt_pwd) \
-                 ''
-    string join ', ' $items
+	if test "$status" -eq 0; set prefix_colour normal
+    else;                    set prefix_colour red
+    end
+	set items  (set_color "$host_colour")(hostname)(set_color normal) \
+               (set -q VIRTUAL_ENV; and basename "$VIRTUAL_ENV env") \
+               (prompt_pwd)
+    set prefix (set_color "$prefix_colour")'■'(set_color normal)
+    printf '%s %s, ' $prefix (string join ', ' $items)
 end
