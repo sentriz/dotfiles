d6dd8385c817bbfa7b56d10f208d1010a1a35639
sentriz <senan@senan.xyz>
2017-11-15 00:14:56 +0000

    change vimode indicator to day of week

---
+++ public_common/.config/fish/functions/fish_mode_prompt.fish
@@ -0,0 +1,4 @@
+# Defined in /tmp/fish.10hkUB/fish_mode_prompt.fish @ line 2
+function fish_mode_prompt --description 'Displays the current mode'
+	
+end
--- public_common/.config/fish/functions/fish_prompt.fish
+++ public_common/.config/fish/functions/fish_prompt.fish
@@ -1,8 +1,8 @@
-# Defined in /tmp/fish.CCTv7q/fish_prompt.fish @ line 2
+# Defined in /tmp/fish.5XjncK/fish_prompt.fish @ line 2
 function fish_prompt
-    set env_string ''
+	set env_string ''
     if set -q VIRTUAL_ENV
         set env_string ', '(basename "$VIRTUAL_ENV")' env'
     end
-    printf (set_color "$host_colour")"%s"(set_color normal)"$env_string, %s, " (hostname) (prompt_pwd)
+    printf "%s, "(set_color "$host_colour")"%s"(set_color normal)"$env_string, %s, " (date +%A | tr '[:upper:]' '[:lower:]') (hostname) (prompt_pwd)
 end
