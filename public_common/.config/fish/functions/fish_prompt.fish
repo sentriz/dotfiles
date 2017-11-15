e1af0946ceb63ea685b229f377cef39306d34d33
sentriz <senan@senan.xyz>
2017-11-15 01:51:36 +0000

    add status 🅱lock as another item in items

--- public_common/.config/fish/functions/fish_prompt.fish
+++ public_common/.config/fish/functions/fish_prompt.fish
@@ -1,11 +1,11 @@
-# Defined in /tmp/fish.b3iMas/fish_prompt.fish @ line 2
+# Defined in /tmp/fish.NcbW5D/fish_prompt.fish @ line 2
 function fish_prompt
 	if test "$status" -eq 0; set prefix_colour normal
     else;                    set prefix_colour red
     end
-	set items  (set_color "$host_colour")(hostname)(set_color normal) \
-               (set -q VIRTUAL_ENV; and basename "$VIRTUAL_ENV env") \
-               (prompt_pwd)
-    set prefix (set_color "$prefix_colour")'■'(set_color normal)
-    printf '%s %s, ' $prefix (string join ', ' $items)
+	set items (set_color "$prefix_colour")'■'(set_color normal) \
+	          (set_color "$host_colour")(hostname)(set_color normal) \
+              (set -q VIRTUAL_ENV; and basename "$VIRTUAL_ENV env") \
+              (prompt_pwd)
+    printf '%s, ' (string join ', ' $items)
 end
