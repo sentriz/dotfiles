166adc32d731b072050f8364325303e3d610c024
sentriz <senan@senan.xyz>
2017-11-15 12:25:59 +0000

    concat " env" to virtualenv properly

--- public_common/.config/fish/functions/fish_prompt.fish
+++ public_common/.config/fish/functions/fish_prompt.fish
@@ -1,11 +1,11 @@
-# Defined in /tmp/fish.NcbW5D/fish_prompt.fish @ line 2
+# Defined in /tmp/fish.PO8LDh/fish_prompt.fish @ line 2
 function fish_prompt
 	if test "$status" -eq 0; set prefix_colour normal
     else;                    set prefix_colour red
     end
 	set items (set_color "$prefix_colour")'■'(set_color normal) \
 	          (set_color "$host_colour")(hostname)(set_color normal) \
-              (set -q VIRTUAL_ENV; and basename "$VIRTUAL_ENV env") \
+              (set -q VIRTUAL_ENV; and printf "%s env" (basename "$VIRTUAL_ENV")) \
               (prompt_pwd)
     printf '%s, ' (string join ', ' $items)
 end
