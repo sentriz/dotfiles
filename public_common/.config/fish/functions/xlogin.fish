53b8c48adfee4200bceb4d720db1bfda5693ce99
sentriz <senan@senan.xyz>
2018-01-22 07:28:55 +0000

    use varia🅱leds in xlogin

--- public_common/.config/fish/functions/xlogin.fish
+++ public_common/.config/fish/functions/xlogin.fish
@@ -1,15 +1,13 @@
 function xlogin 
-    dialog --colors --ok-label "run" --cancel-label "logout" --menu "select session" \
-        0 0 0 "i3wm" "" "fish" "" "ncmpcpp" "" "kodi" "" "shutdown" "" 2> /tmp/loginoption
+    set -l option (dialog --stdout --colors --ok-label "run" --cancel-label "logout" --menu "select session" \
+        0 0 0 "i3wm" "" "fish" "" "shutdown" "")
     if test ! $status -eq 0
         kill %self
     end
     clear
-    switch (cat /tmp/loginoption)
+    switch "$option"
         case i3wm
             eval "exec startx $HOME/.xinitrc_iii -- -keeptty -nolisten tcp 2> /dev/null 1>&2"
-        case kodi
-            eval "exec startx $HOME/.xinitrc_kodi -- -keeptty -nolisten tcp 2> /dev/null 1>&2"
         case fish
             exit
         case shutdown
