2176b51d6819148bb4e5d7b8fb84e107f9b6a62a
sentriz <senan@senan.xyz>
2018-03-24 15:54:33 +0000

    reta🅱 fish_prompt

--- public_common/.config/fish/functions/fish_prompt.fish
+++ public_common/.config/fish/functions/fish_prompt.fish
@@ -1,10 +1,10 @@
-# Defined in /home/senan/.config/fish/functions/fish_prompt.fish @ line 2
+# Defined in /tmp/fish.JqBG4A/fish_prompt.fish @ line 2
 function fish_prompt
-	# if test "$status" -eq 0; set prefix_colour normal
+    # if test "$status" -eq 0; set prefix_colour normal
     # else;                    set prefix_colour red
     # end
-	#         (set_color "$prefix_colour")'■'(set_color normal) \
-	set items (set_color "$host_colour")(hostname)(set_color normal) \
+    #         (set_color "$prefix_colour")'■'(set_color normal) \
+    set items (set_color "$host_colour")(hostname)(set_color normal) \
               (set -q VIRTUAL_ENV; and printf "%s env" (basename "$VIRTUAL_ENV")) \
               (prompt_pwd)
     printf '%s, ' (string join ', ' $items)
