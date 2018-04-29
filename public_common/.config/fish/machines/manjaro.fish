5440960a4cf533672d8bd130ab3bbdefcb1b80e2
sentriz <senan@senan.xyz>
2018-04-29 19:53:23 +0100

    add go path stuff

--- public_common/.config/fish/machines/manjaro.fish
+++ public_common/.config/fish/machines/manjaro.fish
@@ -2,14 +2,16 @@ set_prompt_colour brgreen
 
 # homes
 set -gx JAVA_HOME /usr/lib/jvm/java-8-openjdk/
+set -gx ANDROID_HOME /opt/android-sdk/
 set -gx GEM_HOME (ls -t -U | ruby -e 'puts Gem.user_dir')
 set -gx GEM_PATH $GEM_HOME $GEM_PATH
-set -gx ANDROID_HOME /opt/android-sdk/
+set -gx GOPATH $HOME/go
 
 # paths
 set -gx fish_user_paths $HOME/.local/lib/python*/site-packages/ \
                         $GEM_HOME/bin \
                         /opt/Etcher \
+                        $GOPATH/bin \
                         $fish_user_paths
 
 # includes
