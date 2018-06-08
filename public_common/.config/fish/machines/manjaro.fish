72a33245a68ae73345127bd137c1c9565f60f559
sentriz <senan@senan.xyz>
2018-06-08 12:00:27 +0100

    cleanup manjaro.fish

--- public_common/.config/fish/machines/manjaro.fish
+++ public_common/.config/fish/machines/manjaro.fish
@@ -14,16 +14,15 @@ set -gx fish_user_paths $HOME/.local/lib/python*/site-packages/ \
                         $GOPATH/bin \
                         $fish_user_paths
 
-# includes
 if status --is-login
     start_keychain
 end
+
 if status --is-interactive
     activate_virtualfish
     activate_keychain
 end
 
-# x
 if status --is-login; and test -z $DISPLAY; and test -z $TMUX
     select_x_session
 end
