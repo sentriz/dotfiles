385aecef4fbb02652619cad70cd8f8f0e594918b
sentriz <senan@senan.xyz>
2018-09-10 12:58:25 +0100

    update fish config

--- public_common/.config/fish/functions/go_scrap.fish
+++ public_common/.config/fish/functions/go_scrap.fish
@@ -1,14 +1,14 @@
 # Defined in /tmp/fish.J7c8Eo/go-scrap.fish @ line 2
 function go_scrap
-	set -l scrap_dir ~/scrap
-	switch $argv[1]
-        case n new
-            if test (count $argv) -eq 2
-               vim (mktemp -d $scrap_dir/go.$argv[2].XXX)/main.go 
-            else
-               vim (mktemp -d $scrap_dir/go.XXX)/main.go 
-            end
-        case l last
-            vim $scrap_dir/(ls -Art $scrap_dir | tail -n 1)/main.go
+    set -l scrap_dir ~/scrap
+    switch $argv[1]
+    case n new
+        if test (count $argv) -eq 2
+           vim (mktemp -d $scrap_dir/go.$argv[2].XXX)/main.go
+        else
+           vim (mktemp -d $scrap_dir/go.XXX)/main.go
+        end
+    case l last
+        vim $scrap_dir/(ls -Art $scrap_dir | tail -n 1)/main.go
     end
 end
