218ba91839b8e291d08f66c5567ce7beeeea4b7e
sentriz <senan@senan.xyz>
2018-11-27 14:27:04 +0000

    cleanup work

--- public_common/.config/fish/machines/work.fish
+++ public_common/.config/fish/machines/work.fish
@@ -1,12 +1,12 @@
 set -xg fish_color_host brmagenta
 
 if status --is-login
-    start_keychain &
+    start_keychain
 end
 
 if status --is-interactive
     activate_virtualfish
-    activate_keychain &
+    activate_keychain
 end
 
 # aliases
@@ -15,6 +15,7 @@ alias work_paste "curl -sF c=@- pb | grep -Po --color=never 'url:\s\K(.*)'"
 # env
 set -xg ANDROID_NDK /opt/android-ndk
 set -xg ANDROID_NDK_HOME /opt/android-ndk
+set -xg QT_DIR $HOME/qt
 
 if status --is-login; and test -z $DISPLAY; and test -z $TMUX
     select_x_session
