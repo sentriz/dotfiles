ecd8bbd2679e9bfeac364149ba87f01e115822fb
sentriz <senan@senan.xyz>
2019-02-12 14:35:24 +0000

    fish: clean up l/ll

--- public_common/.config/fish/functions/l.fish
+++ public_common/.config/fish/functions/l.fish
@@ -1,5 +1,5 @@
 function l
 	test -d .git; and git status; and echo
     ls -vFqrloth --group-directories-first --color=yes --time-style=long-iso $argv \
-    | sed "s/"(date +%Y-%m-%d)"/\x1b[32m     TODAY\x1b[m/; s/"(date +'%Y-%m-%d' -d yesterday)"/\x1b[33m YESTERDAY\x1b[m/"
+    | sed "s/"(date +%Y-%m-%d)"/\x1b[32m     today\x1b[m/; s/"(date +'%Y-%m-%d' -d yesterday)"/\x1b[33m yesterday\x1b[m/"
 end
