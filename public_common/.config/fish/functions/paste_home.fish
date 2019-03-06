f1e9b39f6be5e55c1a8b4ac174ae6f6a4b1f35db
sentriz <senan@senan.xyz>
2019-03-06 11:58:04 +0000

    fish: change paste_home url

--- public_common/.config/fish/functions/paste_home.fish
+++ public_common/.config/fish/functions/paste_home.fish
@@ -1,5 +1,5 @@
 function paste_home
-    set -l haste_url "https://haste.home.senan.xyz"
+    set -l haste_url "https://paste.home.senan.xyz"
     read -lz data
     set -l key (curl -sf --data-binary "$data" $haste_url"/documents" | jq -r '.key')
     printf "%s/%s\n" $haste_url $key
