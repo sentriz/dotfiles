4bfd64cbe2d29eba051584f9c09c04aab645b985
sentriz <senan@senan.xyz>
2019-07-31 18:56:05 +0100

    fish: use gpg-agent for ssh

--- public_common/.config/fish/functions/activate_keychain.fish
+++ public_common/.config/fish/functions/activate_keychain.fish
@@ -1,9 +1,6 @@
 # Defined in /tmp/fish.dxZgk4/activate_keychain.fish @ line 2
 function activate_keychain
-    if test -f ~/.keychain/(hostname)-fish-gpg
-        source ~/.keychain/(hostname)-fish-gpg
-    end
-    if test -f ~/.keychain/(hostname)-fish
-        source ~/.keychain/(hostname)-fish
-    end
+    gpgconf --launch gpg-agent
+    set -gx GPG_TTY (tty)
+    set -gx SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
 end
