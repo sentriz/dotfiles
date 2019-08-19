ec647b8f5105379392e249838cbb76af69dd96a4
sentriz <senan@senan.xyz>
2019-08-19 20:34:46 +0100

    fish: set gnupg display per shell

--- public_common/.config/fish/functions/activate_keychain.fish
+++ public_common/.config/fish/functions/activate_keychain.fish
@@ -1,5 +1,5 @@
 function activate_keychain
-    gpgconf --launch gpg-agent
     set -gx GPG_TTY (tty)
     set -gx SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+    gpg-connect-agent updatestartuptty /bye >/dev/null
 end
