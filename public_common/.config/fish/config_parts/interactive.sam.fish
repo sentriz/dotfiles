2d5710d9d3614e14b59d7389b4b3d13cbb0291a6
sentriz <senan@senan.xyz>
2019-10-11 13:47:36 +0100

    fish: add cc command for sam

--- public_common/.config/fish/config_parts/interactive.sam.fish
+++ public_common/.config/fish/config_parts/interactive.sam.fish
@@ -1,1 +1,18 @@
 set -xg fish_color_host blue
+
+# jump to my containers easily with completion
+set -g container_dir "/opt/containers"
+function cc --argument container
+    cd "$container_dir/$container"
+end
+# need to wrap the inner subshell, so using this extra echo subshell
+complete \
+    --command cc \
+    --arguments (echo (cd "$container_dir"; find \
+         . \
+         -maxdepth 1 \
+         -mindepth 1 \
+         -type d \
+         -regextype egrep \
+         ! -regex '^\./(\.|_).*' -printf '%P\n'
+    ))
