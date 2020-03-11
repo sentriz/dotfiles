986b61fe9124d98865a2c53945b3103a9e32b30c
sentriz <senan@senan.xyz>
2020-03-11 00:44:30 +0000

    fish: add termux support

--- public_common/.config/fish/config.fish
+++ public_common/.config/fish/config.fish
@@ -5,14 +5,20 @@ function __source_if_exists
     end
 end
 
+if string match -q -r '.*termux.*' "$HOME"
+    set -gx HOSTNAME android
+else
+    set -gx HOSTNAME (hostname)
+end
+
 __source_if_exists \
     ~/.config/fish/config_parts/base.fish \
-    ~/.config/fish/config_parts/base.(hostname).fish
+    ~/.config/fish/config_parts/base.$HOSTNAME.fish
 
 status --is-login; and __source_if_exists \
     ~/.config/fish/config_parts/login.fish \
-    ~/.config/fish/config_parts/login.(hostname).fish
+    ~/.config/fish/config_parts/login.$HOSTNAME.fish
 
 status --is-interactive; and __source_if_exists \
     ~/.config/fish/config_parts/interactive.fish \
-    ~/.config/fish/config_parts/interactive.(hostname).fish
+    ~/.config/fish/config_parts/interactive.$HOSTNAME.fish
--- public_common/.config/fish/functions/fish_prompt.fish
+++ public_common/.config/fish/functions/fish_prompt.fish
@@ -1,6 +1,6 @@
 function fish_prompt
     set items \
-        (printf '%s%s%s' (set_color "$fish_color_host") (hostname) (set_color normal)) \
+        (printf '%s%s%s' (set_color "$fish_color_host") "$HOSTNAME" (set_color normal)) \
         (set -q VIRTUAL_ENV; and printf "%s env" (basename "$VIRTUAL_ENV")) \
         (prompt_pwd)
     printf '%s, ' (string join ', ' $items)
