1123c1b8fe2054ed4a9190d3943469798bdeee16
sentriz <senan@senan.xyz>
2020-04-19 11:28:08 +0100

    fish: add git prompt back

--- conf_common/.config/fish/functions/fish_right_prompt.fish
+++ conf_common/.config/fish/functions/fish_right_prompt.fish
@@ -1,2 +1,12 @@
 function fish_right_prompt
+    set -l branch (git symbolic-ref --quiet --short HEAD 2> /dev/null)
+    test -z "$branch"; and return
+    test "$branch" = master
+        and set_color brred
+        or set_color brgreen
+    echo "$branch"
+    set_color normal
+    git diff-index --quiet HEAD --
+        and echo " clean"
+        or echo " dirty"
 end
