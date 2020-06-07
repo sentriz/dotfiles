06c2a6d3a21b68ff7f319c5c0fa91783ea0efce2
sentriz <senan@senan.xyz>
2020-06-07 03:27:41 +0100

    fish: refactor sway startup and add htop on tty2

--- conf_common/.config/fish/config_parts/login.manjaro.fish
+++ conf_common/.config/fish/config_parts/login.manjaro.fish
@@ -1,11 +1,6 @@
-test (tty) != '/dev/tty1'; and exit
-
-set -gx XDG_CONFIG_HOME "$HOME/.config"
-set -gx XDG_DATA_DIRS (string join ':' \
-    '/usr/local/share' \
-    '/usr/share' \
-    '/var/lib/flatpak/exports/share' \
-    '/home/senan/.local/share/flatpak/exports/share'
-)
-
-exec start-sway
+switch (tty)
+    case '/dev/tty1'
+        exec start-sway
+    case '/dev/tty2'
+        exec htop
+end
