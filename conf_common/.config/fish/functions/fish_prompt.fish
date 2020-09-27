e2647e12ff135f638219d6f8211c6e2324976c52
sentriz <senan@senan.xyz>
2020-09-27 20:08:47 +0100

    fish: update fish_prompt

--- conf_common/.config/fish/functions/fish_prompt.fish
+++ conf_common/.config/fish/functions/fish_prompt.fish
@@ -1,7 +1,9 @@
+# Defined in /tmp/fish.yVO8Iu/fish_prompt.fish @ line 2
 function fish_prompt
-    printf '%s%s%s %s ' \
+    printf '%s%s %s%s %s' \
         (set_color "$fish_color_host") \
         "$HOSTNAME" \
-        (set_color normal) \
-        (prompt_pwd)
+        (set_color --dim white) \
+        (prompt_pwd) \
+        (set_color normal)
 end
