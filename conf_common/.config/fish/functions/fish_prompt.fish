85f50c70b18a58dfc09be657184c8c5320469f0f
sentriz <senan@senan.xyz>
2021-04-01 18:17:12 +0100

    fish: show error status

--- conf_common/.config/fish/functions/fish_prompt.fish
+++ conf_common/.config/fish/functions/fish_prompt.fish
@@ -1,5 +1,9 @@
 # Defined in /tmp/fish.yVO8Iu/fish_prompt.fish @ line 2
 function fish_prompt
+    test "$status" -gt 0
+        and set_color brred
+        and echo -n "██ "
+
     set_color "$fish_color_host"
     echo -n "$HOSTNAME"
 
