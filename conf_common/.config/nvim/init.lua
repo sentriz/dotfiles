9b5af3c1401fdd563de18c7216d5c1db8302124a
sentriz <senan@senan.xyz>
2023-02-01 15:03:56 +0000

    vim: don't return .d.ts results for tsserver go-to-definition
    https://github.com/typescript-language-server/typescript-language-server/issues/216

--- conf_common/.config/nvim/init.lua
+++ conf_common/.config/nvim/init.lua
@@ -367,6 +367,14 @@ lspconfig.denols.setup({
 lspconfig.tsserver.setup({
 	capabilities = capabilities,
 	root_dir = util.root_pattern("tsconfig.json", "jsconfig.json"),
+	handlers = {
+		-- pick the first response to a go to definition response. that way we go straight to the
+		-- source definition without needing to choose from the type definition .d.ts file
+		["textDocument/definition"] = function(err, result, ...)
+			result = vim.tbl_islist(result) and result[1] or result
+			vim.lsp.handlers["textDocument/definition"](err, result, ...)
+		end,
+	},
 })
 
 lspconfig.jdtls.setup({
