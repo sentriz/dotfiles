f249e017d35c7a4a493369dbf4545493bd4612d5
sentriz <senan@senan.xyz>
2023-06-22 19:16:00 +0100

    fish: shut git up

--- conf_common/.config/fish/functions/fish_right_prompt.fish
+++ conf_common/.config/fish/functions/fish_right_prompt.fish
@@ -2,7 +2,7 @@ function fish_right_prompt
     set -l branch (git symbolic-ref --quiet --short HEAD 2>/dev/null)
     test -z "$branch"; and return
 
-    set -l count (git diff-index HEAD -- | wc -l)
+    set -l count (git diff-index HEAD -- 2>/dev/null | wc -l)
     test $count -eq 1; and echo "$count change "
     test $count -gt 1; and echo "$count changes "
 
