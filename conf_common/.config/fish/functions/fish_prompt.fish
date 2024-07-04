1523f5e3a9cc6d1ec85105ac85cec9121c6d3799
sentriz <senan@senan.xyz>
2024-07-04 20:04:24 +0100

    fish: show background jobs in prompt

--- conf_common/.config/fish/functions/fish_prompt.fish
+++ conf_common/.config/fish/functions/fish_prompt.fish
@@ -1,8 +1,12 @@
 # Defined in /tmp/fish.yVO8Iu/fish_prompt.fish @ line 2
 function fish_prompt
-    set prev_status "$status"
-    set sh_lvl (math "$SHLVL" - 1)
+    set prev_status "$status" # needs to be first
 
+    set jc (jobs | wc -l)
+    test "$jc" -gt 0
+    and echo -n "$jc "
+
+    set sh_lvl (math "$SHLVL" - 1)
     test "$sh_lvl" -gt 0
     and echo -n (string repeat -N -n $sh_lvl ">")" "
 
