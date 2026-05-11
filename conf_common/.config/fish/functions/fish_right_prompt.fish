b2234660762018cc6f20f538ef50a5b659b22999
sentriz <senan@senan.xyz>
2026-05-11 16:57:45 +0100

    fish: print fish_right_prompt on one line

--- conf_common/.config/fish/functions/fish_right_prompt.fish
+++ conf_common/.config/fish/functions/fish_right_prompt.fish
@@ -3,13 +3,13 @@ function fish_right_prompt
     test -z "$branch"; and return
 
     set -l count (git diff-index HEAD -- 2>/dev/null | wc -l)
-    test $count -eq 1; and echo "$count change "
-    test $count -gt 1; and echo "$count changes "
+    test $count -eq 1; and echo -n "$count change "
+    test $count -gt 1; and echo -n "$count changes "
 
     test "$branch" = master
     and set_color brred
     or set_color brgreen
-    echo "$branch"
+    echo -n "$branch"
 
     set_color normal
 end
