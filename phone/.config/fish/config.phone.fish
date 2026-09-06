8a799c3b72b9593157874f707252aec59d5f49dc
sentriz <senan@senan.xyz>
2026-09-06 14:17:36 +0100

    phone: start sshd and the secrets daemon from the login shell

--- phone/.config/fish/config.phone.fish
+++ phone/.config/fish/config.phone.fish
@@ -2,3 +2,9 @@ set -gx fish_colour_host brmagenta
 
 set -gx SSH_AUTH_SOCK "$TMPDIR/rbw-"(id -u)"/ssh-agent-socket"
 set -gx SECRETS_SOCK "$TMPDIR/secrets-socket"
+
+if status is-login
+    pgrep -x sshd >/dev/null; or sshd
+    test -S "$SSH_AUTH_SOCK"; or pkill -x rbw-agent
+    secret daemon
+end
